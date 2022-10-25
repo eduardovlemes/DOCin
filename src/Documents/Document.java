c38794cd47461d4f87d4347755354004910f8cfc
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-25 16:49:16 -0300

    feat: cria a classe Document
    Cria a classe que cria o documento.

--- src/Documents/Document.java
+++ src/Documents/Document.java
@@ -1,4 +1,21 @@
-package Documents;
-
+package documents;
 public class Document {
+    private Integer documentId;
+    private Integer supervisorId;
+    private Integer personId;
+    private String documentTitle;
+    private String documentLink;
+    private String documentState;
+    public Document(Integer documentId, Integer supervisorId, Integer personId, String documentTitle, String documentLink, String documentState){
+        this.documentId = documentId;
+        this.supervisorId = supervisorId;
+        this.personId = personId;
+        this.documentTitle = documentTitle;
+        this.documentLink = documentLink;
+        this.documentState = documentState;
+    }
+    @Override
+    public String toString(){
+        return "Documento{" + "Título do documento="+ documentTitle + '\'' + ",Link do documento=" + documentLink + '\'' + ", Estado do documento=" + documentState + ", Identificador do documento=" + documentId + ", Identificador do criador=" + personId + ", Identificador do responsável=" + supervisorId + '}';
+    }
 }
