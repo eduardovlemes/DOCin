67529182b2c7f408158684773af2466802408bdb
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-25 16:48:10 -0300

    feat: cria a classe Employee
    Cria a classe que recebe o empregado, extendido de collaborator.

--- src/Persons/Employee.java
+++ src/Persons/Employee.java
@@ -1,4 +1,21 @@
-package Persons;
+package persons;
 
-public class Employee {
+import documents.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employee extends Collaborator{
+    private List<Document> documents = new ArrayList<>();
+
+    public Employee (String name, String lastName, Integer personId){
+        super(name, lastName, personId);
+    }
+    @Override
+    public void CreateDocument(Document document){
+        documents.add(document);
+        System.out.println("Documento criado");
+        System.out.println(document);
+        System.out.println(documents);
+    }
 }
