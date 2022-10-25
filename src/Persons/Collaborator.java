ee8185f9c7645f60de614625243d326aeadf73c1
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-25 16:45:53 -0300

    feat: cria classe Collaborator
    Cria a classe Collaborator que extende os dados de Person. Bem como adiciona o colaborador no PersonList.

--- src/Persons/Collaborator.java
+++ src/Persons/Collaborator.java
@@ -1,4 +1,21 @@
-package Persons;
+package persons;
 
-public class Collaborator {
+import documents.Document;
+import entities.PersonList;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Collaborator extends Person{
+    private List<Document> documents = new ArrayList<>();
+
+    public Collaborator(String name, String lastName, Integer personId){
+        super(name, lastName, personId);
+        PersonList.addPerson(this);
+    }
+
+    public void CreateDocument(Document document){
+        documents.add(document);
+        System.out.println("Documento criado");
+    }
 }
