6769d41fb46ed70b79063bec5a53af2f00a65d5a
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-25 16:50:19 -0300

    feat: cria a classe Supervisor
    Cria a classe supervisor que extende dados de collaborator e que aprova os documentos.

--- src/Persons/Supervisor.java
+++ src/Persons/Supervisor.java
@@ -1,4 +1,20 @@
-package Persons;
+package persons;
 
-public class Supervisor {
+import documents.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Supervisor extends Collaborator {
+    private static List<Document> approvedDocuments = new ArrayList<>();
+
+    public Supervisor(String name, String lastName, Integer personId){
+        super(name, lastName, personId);
+    }
+    @Override
+    public void approveDocuments (Document document){
+        System.out.println("Documento aprovado pelo supervisor.");
+        approvedDocuments.add(document);
+        System.out.println(approvedDocuments);
+    }
 }
