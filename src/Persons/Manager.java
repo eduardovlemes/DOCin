652756d41557dfa11ca4221cf609477899770f08
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-25 16:51:00 -0300

    feat: cria a classe Manager
    Cria a classe que arquiva os documentos.

--- src/Persons/Manager.java
+++ src/Persons/Manager.java
@@ -1,4 +1,19 @@
-package Persons;
+package persons;
 
-public class Manager {
+import documents.Document;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Manager extends Collaborator{
+    private static List<Document> archivedDocuments = new ArrayList<>();
+
+    public Manager(String name, String lastName, Integer personId){
+        super(name, lastName, personId);
+    }
+    @Override
+    public void archiveDocuments (Document document){
+        System.out.println("Documento arquivado pelo gerente.");
+        archivedDocuments.add(document);
+    }
 }
