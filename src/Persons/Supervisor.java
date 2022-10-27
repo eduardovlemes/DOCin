41ca1e016acde82c1726b6dff9bfccd323a7f514
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-27 15:41:08 -0300

    feat: ajuste class Supervisor
    adiciona contador, ajusta construtor e implementa novo método.

--- src/Persons/Supervisor.java
+++ src/Persons/Supervisor.java
@@ -1,20 +1,31 @@
 package persons;
 
 import documents.Document;
+import entities.EmployeeList;
+import entities.SupervisorList;
 
+import javax.print.Doc;
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Supervisor extends Collaborator {
+    private static Integer counter = 0;
+
+    private static List<Document> pendingDocuments = new ArrayList<>();
     private static List<Document> approvedDocuments = new ArrayList<>();
 
-    public Supervisor(String name, String lastName, Integer personId){
-        super(name, lastName, personId);
+    public Supervisor(String firstname, String lastName, LocalDate birthday, Double cpf, String address){
+        super(firstname, lastName, birthday, cpf, address,counter++);
+        SupervisorList.addSupervisor(this);
     }
     @Override
-    public void approveDocuments (Document document){
-        System.out.println("Documento aprovado pelo supervisor.");
-        approvedDocuments.add(document);
-        System.out.println(approvedDocuments);
+    public void addDocuments (Document document){
+        pendingDocuments.remove(document);
     }
+    public void approveDocuments (Document document){
+        System.out.println("Documento aprovado pelo supervisor, nome:" + this.getFirstname());
+        approvedDocuments.add(document);
+    }
+
 }
