6d081b167c7ddcda369c89e32a82a02170af8162
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-27 15:43:58 -0300

    feat: arruma construtor e traz todos os métodos

--- src/Persons/Collaborator.java
+++ src/Persons/Collaborator.java
@@ -3,19 +3,18 @@ package persons;
 import documents.Document;
 import entities.PersonList;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Collaborator extends Person{
     private List<Document> documents = new ArrayList<>();
-
-    public Collaborator(String name, String lastName, Integer personId){
-        super(name, lastName, personId);
-        PersonList.addPerson(this);
+    public Collaborator(String firstname, String lastName, LocalDate birthday, Double cpf, String address, Integer id) {
+        super(firstname, lastName, birthday, cpf, address, id);
+        PersonList.addCollaborator(this);
     }
-
-    public void CreateDocument(Document document){
-        documents.add(document);
-        System.out.println("Documento criado");
-    }
+    public void addDocuments(Document document){}
+    public void requestApproval(Integer idSupervisor, Integer idDocument){}
+    public void approveDocuments (Document document){}
+    public void archiveDocuments (Document document){}
 }
