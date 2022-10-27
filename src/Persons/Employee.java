6c75559c3e6b6d846d67084fb3b96fe381b4fb49
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-27 15:43:14 -0300

    feat: arruma construtor

--- src/Persons/Employee.java
+++ src/Persons/Employee.java
@@ -1,21 +1,30 @@
 package persons;
 
 import documents.Document;
+import entities.EmployeeList;
+import entities.SupervisorList;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Employee extends Collaborator{
+    private static Integer counter = 0;
     private List<Document> documents = new ArrayList<>();
 
-    public Employee (String name, String lastName, Integer personId){
-        super(name, lastName, personId);
+    public Employee(String firstname, String lastName, LocalDate birthday, Double cpf, String address) {
+        super(firstname, lastName, birthday, cpf, address,counter++);
+        EmployeeList.addEmployee(this);
     }
     @Override
-    public void CreateDocument(Document document){
+    public void addDocuments(Document document){
         documents.add(document);
-        System.out.println("Documento criado");
+        System.out.println("Documento criado pelo funcionário");
         System.out.println(document);
         System.out.println(documents);
+    }
+    public void requestApproval(Integer idSupervisor, Integer idDocument){
+        Supervisor approver = SupervisorList.returnSupervisor(idSupervisor);
+        approver.pendingDocuments(documents.get(idDocument));
     }
 }
