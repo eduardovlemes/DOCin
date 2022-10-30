32442fde796971886b8b738b03656352223cd134
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-29 21:29:19 -0300

    feat: remove itens não usados

--- src/Persons/Collaborator.java
+++ src/Persons/Collaborator.java
@@ -9,12 +9,11 @@ import java.util.List;
 
 public class Collaborator extends Person{
     private List<Document> documents = new ArrayList<>();
-    public Collaborator(String firstname, String lastName, LocalDate birthday, Double cpf, String address, Integer id) {
+    public Collaborator(String firstname, String lastName, LocalDate birthday, String cpf, String address, Integer id) {
         super(firstname, lastName, birthday, cpf, address, id);
         PersonList.addCollaborator(this);
     }
-    public void addDocuments(Document document){}
-    public void requestApproval(Integer idSupervisor, Integer idDocument){}
-    public void approveDocuments (Document document){}
-    public void archiveDocuments (Document document){}
+    public void addDocument(Document document){}
+
+
 }
