073de027ed9ea1c08a88e25f1abb95ebd838c71f
Eduardo V. Lemes <91350022+eduardovlemes@users.noreply.github.com>
2022-10-29 21:32:15 -0300

    feat: adiciona funções adicionar, aprovar e rejeitar

--- src/Persons/Supervisor.java
+++ src/Persons/Supervisor.java
@@ -4,28 +4,56 @@ import documents.Document;
 import entities.EmployeeList;
 import entities.SupervisorList;
 
-import javax.print.Doc;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+
 
 public class Supervisor extends Collaborator {
     private static Integer counter = 0;
-
     private static List<Document> pendingDocuments = new ArrayList<>();
     private static List<Document> approvedDocuments = new ArrayList<>();
 
-    public Supervisor(String firstname, String lastName, LocalDate birthday, Double cpf, String address){
+
+
+
+
+    public Supervisor(String firstname, String lastName, LocalDate birthday, String cpf, String address){
         super(firstname, lastName, birthday, cpf, address,counter++);
         SupervisorList.addSupervisor(this);
     }
+
+
+
+
     @Override
-    public void addDocuments (Document document){
-        pendingDocuments.remove(document);
+    public void addDocument(Document document){
+        pendingDocuments.add(document);
+        System.out.println("Documento criado pelo funcionário, nome:" + this.getFirstname());
+        System.out.println(pendingDocuments);
     }
-    public void approveDocuments (Document document){
+
+    public void approveDocument(Integer documentId){
         System.out.println("Documento aprovado pelo supervisor, nome:" + this.getFirstname());
-        approvedDocuments.add(document);
+        for (Document document: pendingDocuments) {
+            if (Objects.equals(document.getDocumentId(),documentId)){
+                pendingDocuments.remove(document);
+                document.setDocumentTitle("");
+                approvedDocuments.add(document);
+            }
+        }
+    }
+
+    public void rejectDocument(Integer documentId, Integer employeeId){
+        Employee employee = EmployeeList.returnEmployee(employeeId);
+
+        for (Document document:pendingDocuments) {
+            if (Objects.equals(document.getDocumentId(),documentId)){
+                employee.addDocument(document);
+                pendingDocuments.remove(document);
+            }
+        }
     }
 
 }
